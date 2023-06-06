880bfcff42e7672dea4db718914045556688f2f0
arief0303 <arief0303@gmail.com>
2023-06-06 19:42:26 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import * as BABYLON from 'babylonjs';
+import Stats from 'stats-js';
+
+var statsFPS = new Stats();
+statsFPS.domElement.style.cssText = "position:absolute;top:3px;left:3px;";
+statsFPS.showPanel(0); // 0: fps,
+
+var statsMemory = new Stats();
+statsMemory.showPanel(2); //2: mb, 1: ms, 3+: custom
+statsMemory.domElement.style.cssText = "position:absolute;top:3px;left:84px;";
+
+//add stats for FPS and Memory usage
+document.body.appendChild(statsFPS.dom);
+document.body.appendChild(statsMemory.dom);
 
 function App() {
+  const canvasRef = React.useRef(null);
+
+  React.useEffect(() => {
+    const canvas = canvasRef.current;
+    const engine = new BABYLON.Engine(canvas, true);
+
+    const createScene = () => {
+      const scene = new BABYLON.Scene(engine);
+      const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 5, -10), scene);
+      camera.setTarget(BABYLON.Vector3.Zero());
+      camera.attachControl(canvas, true);
+
+      new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
+
+      BABYLON.MeshBuilder.CreateSphere('sphere', { diameter: 2 }, scene);
+
+      return scene;
+    };
+
+    const scene = createScene();
+
+    engine.runRenderLoop(() => {
+      scene.render();
+      statsFPS.update();
+      statsMemory.update();
+    });
+
+    const resize = () => {
+      engine.resize();
+    };
+
+    window.addEventListener('resize', resize);
+
+    return () => {
+      window.removeEventListener('resize', resize);
+      scene.dispose();
+      engine.dispose();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas ref={canvasRef} style={{ width: '100vw', height: '100vh' }} />
   );
 }
 
